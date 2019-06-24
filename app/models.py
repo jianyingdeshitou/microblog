69a8d702a6c91c24b5526954e50f9c4ef04d58c4
坚硬的石头 <do_nb@outlook.com>
2019-06-24 10:26:03 +0800

    第6章个人资料页面和头像：User 添加 about_me 和 last_seen 字段

--- app/models.py
+++ app/models.py
@@ -22,6 +22,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     # 密码哈希值
     password_hash = db.Column(db.String(128))
+    # 个人简介
+    about_me = db.Column(db.String(140))
+    # 最后访问时间
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)

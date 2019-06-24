914fe999834f63c6133cdeab93dcbe8e7e444b62
坚硬的石头 <do_nb@outlook.com>
2019-06-24 10:29:39 +0800

    第6章个人资料页面和头像：记录用户的上次访问时间

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for
 from flask import request
 
@@ -101,3 +103,9 @@ def user(username):
 
 
 ###############################################################################
+# 获取上次访问时间
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()

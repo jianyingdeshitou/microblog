b4d1d611f38db0d95ed12b3a3beb0a12fcd84113
坚硬的石头 <do_nb@outlook.com>
2019-06-24 11:11:30 +0800

    第8章：粉丝：获取被关注用户的帖子

--- app/models.py
+++ app/models.py
@@ -73,6 +73,13 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
+    # 获取被关注用户的帖子
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 #######################################################################
 
 

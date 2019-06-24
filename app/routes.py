96e711ea87f7c091ecacb71192085ea73f516378
坚硬的石头 <do_nb@outlook.com>
2019-06-24 08:27:58 +0800

    第5章用户登录：添加注册用户页面

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,11 @@ from flask_login import login_required
 from werkzeug.urls import url_parse
 
 from app import app
+from app import db
 from app.forms import LoginForm
+from app.forms import RegistrationForm
 from app.models import User
+
 
 ###############################################################################
 @app.route('/')
@@ -64,3 +67,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+###############################################################################
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+

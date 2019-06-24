ab9907486abe72a77409cb9142a7da05d30edbe3
坚硬的石头 <do_nb@outlook.com>
2019-06-24 13:15:32 +0800

    第8章：粉丝：添加帖子分页

--- config.py
+++ config.py
@@ -5,8 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 os.environ['wsgi.url_scheme'] = 'https'
 
 class Config(object):
+    # 加密设置
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    # 数据库设置
     DB_HOST = os.environ.get('DB_HOST')
     DB_ROOT_PASSWORD = os.environ.get('DB_ROOT_PASSWORD')
     DB_NAME = os.environ.get('DB_NAME')
@@ -16,9 +18,14 @@ class Config(object):
         (DB_ROOT_PASSWORD, DB_HOST, DB_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # 邮件设置
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+
+    # 分页
+    POSTS_PER_PAGE = 3
+

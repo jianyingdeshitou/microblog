2811b590f87e14e0f7ab6024fac42870cf638215
坚硬的石头 <do_nb@outlook.com>
2019-06-24 08:16:20 +0800

    第5章用户登录： 添加登录验证，解决url跳转不是https问题

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,27 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_login import LoginManager
+
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,20 @@
 from flask import render_template, flash, redirect, url_for
+from flask import request
+
+from flask_login import current_user, login_user
+from flask_login import logout_user
+from flask_login import login_required
+
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 ###############################################################################
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -18,16 +27,40 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
 
 
 ###############################################################################
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    # 如果当前用户已经登录
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+    # 验证用户登录
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get('next')
+        # 1. 如果登录URL没有next参数，则将用户重定向到索引页面。
+        # 2. 如果登录URL包含next设置为包含域名的完整URL 的参数，则会将用户重定向到索引页。
+        # 攻击者可以在next参数中向恶意站点插入URL ，
+        # 因此应用程序仅在URL为相对时重定向，
+        # 这可确保重定向与应用程序保持在同一站点内。
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        # 3. 如果登录URL包含next设置为相对路径的参数（或者换句话说，没有域部分的URL），
+        # 则将用户重定向到该URL。
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+###############################################################################
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
